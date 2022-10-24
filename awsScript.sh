a1268fb613409649d078bc8604ea11316ab4fd8c
pr57039n <107935666+pr57039n@users.noreply.github.com>
2022-10-24 11:05:43 -0400

    Update awsScript.sh

--- awsScript.sh
+++ awsScript.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 cd $HOME
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zi>
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip>
 sudo apt-get -y install unzip
 unzip awscliv2.zip
 sudo ./aws/install
 aws --version
-cd /Repositories
+cd /Repositories/AutoDeployment2Test
 sudo chmod 755 JenkinsUser.sh
 sudo ./JenkinsUser.sh
