baee55fe685f8530c22a3fe487e3f25ca508541d
pr57039n <107935666+pr57039n@users.noreply.github.com>
2022-10-24 09:38:14 -0400

    Create jenkinsScript.sh

---
+++ jenkinsScript.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ $UID != 0]; then
+        echo "Not an admin, exiting out"
+        exit 1
+fi
+
+sudo apt-get update -y
+sudo apt -y install openjdk-11-jre
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee /usr/>
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkin>
+sudo apt-get update
+sudo apt-get -y install jenkins
+sudo systemctl start jenkins
+
+sudo apt-get install python3pip -y &&  sudo apt-get install python3.10-venv -y
+sudo apt-get install git -y
