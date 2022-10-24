b7941257723800bda76483d967da5d4ebe509b02
pr57039n <107935666+pr57039n@users.noreply.github.com>
2022-10-24 10:22:34 -0400

    Update jenkinsScript.sh

--- jenkinsScript.sh
+++ jenkinsScript.sh
@@ -6,12 +6,11 @@ if [ $UID != 0]; then
 fi
 sudo apt-get update -y
 sudo apt -y install openjdk-11-jre
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee /usr/>
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkin>
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt-get update
 sudo apt-get -y install jenkins
 sudo systemctl start jenkins
-
 sudo apt-get install python3pip -y &&  sudo apt-get install python3.10-venv -y
 sudo apt-get install git -y
 cd /
